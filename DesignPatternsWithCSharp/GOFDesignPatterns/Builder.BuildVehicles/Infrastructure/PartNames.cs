3e45e1187845c6b51d3693b17381b168b2ab7957
vishipayyallore <vishipayyallore@yahoo.com>
2015-09-11 21:22:14 +0530

    AAA

---
+++ DesignPatternsWithCSharp/GOFDesignPatterns/Builder.BuildVehicles/Infrastructure/PartNames.cs
@@ -0,0 +1,14 @@
+﻿namespace Builder.BuildVehicles.Infrastructure
+{
+
+    public class PartNames
+    {
+        #region Properties
+        public const string Frame = "Frame";
+        public const string Engine = "Engine";
+        public const string Wheels = "Wheels";
+        public const string Doors = "Doors";
+        #endregion
+    }
+
+}
--- DesignPatternsWithCSharp/GOFDesignPatterns/Builder.BuildVehicles/Product/Vehicle.cs
+++ DesignPatternsWithCSharp/GOFDesignPatterns/Builder.BuildVehicles/Product/Vehicle.cs
@@ -1,4 +1,7 @@
-﻿using Builder.BuildVehicles.Infrastructure;
+﻿using System;
+using System.Collections.Generic;
+using Builder.BuildVehicles.Infrastructure;
+using Builder.BuildVehicles.VehicleParts;
 
 namespace Builder.BuildVehicles.Product
 {
@@ -6,7 +9,8 @@ namespace Builder.BuildVehicles.Product
     public class Vehicle
     {
         #region Variables.
-        private VehicleTypes _vehicleType;
+        private readonly VehicleTypes _vehicleType;
+        private readonly Dictionary<string, Part> _parts =  new Dictionary<string, Part>();
         #endregion
 
         public Vehicle(VehicleTypes vehicleType)
@@ -15,11 +19,20 @@ namespace Builder.BuildVehicles.Product
         }
 
         #region Indexer
-
+        public Part this[string partName]
+        {
+            get { return _parts[partName]; }
+            set { _parts[partName] = value; }
+        }
         #endregion
 
         #region Methods
-
+        public void DisplayVehicle()
+        {
+            Console.WriteLine("\n---------------------------");
+            Console.WriteLine("Vehicle Type: {0}", _vehicleType);
+            Console.WriteLine(" Frame  : {0}", _parts[PartNames.Frame]);
+        }
         #endregion
     }
 
