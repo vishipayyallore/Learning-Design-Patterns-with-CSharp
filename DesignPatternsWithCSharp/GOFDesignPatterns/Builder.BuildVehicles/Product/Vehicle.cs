522da5c6aa45f597a8ce4219a5382acdea44419c
vishipayyallore <vishipayyallore@yahoo.com>
2015-09-11 21:12:13 +0530

    AAA

---
+++ DesignPatternsWithCSharp/GOFDesignPatterns/Builder.BuildVehicles/Product/Vehicle.cs
@@ -0,0 +1,26 @@
+﻿using Builder.BuildVehicles.Infrastructure;
+
+namespace Builder.BuildVehicles.Product
+{
+
+    public class Vehicle
+    {
+        #region Variables.
+        private VehicleTypes _vehicleType;
+        #endregion
+
+        public Vehicle(VehicleTypes vehicleType)
+        {
+            _vehicleType = vehicleType;
+        }
+
+        #region Indexer
+
+        #endregion
+
+        #region Methods
+
+        #endregion
+    }
+
+}
