121a5599a52b19f5409c326d4448161337dc5e8c
vishipayyallore <vishipayyallore@yahoo.com>
2015-09-12 23:21:56 +0530

    After Director class and its tests are added.

---
+++ DesignPatternsWithCSharp/GOFDesignPatterns/Builder.BuildVehicles/Director/Shop.cs
@@ -0,0 +1,21 @@
+﻿using Builder.BuildVehicles.Builder;
+
+namespace Builder.BuildVehicles.Director
+{
+
+    public class Shop
+    {
+        #region Methods.
+        public void Construct(VehiclesBuilder vehiclesBuilder)
+        {
+            vehiclesBuilder
+                .BuildFrame()
+                .BuildEngine()
+                .BuildWheels()
+                .BuildDoors()
+                .DisplayVehicle();
+        }
+        #endregion
+    }
+
+}
---
+++ DesignPatternsWithCSharp/GOFDesignPatterns/Builder.BuildVehiclesTests/ShopTests.cs
@@ -0,0 +1,36 @@
+﻿using Builder.BuildVehicles.ConcreteBuilder;
+using Builder.BuildVehicles.Director;
+using Builder.BuildVehicles.Infrastructure;
+using Builder.BuildVehicles.Product;
+using Builder.BuildVehicles.VehicleParts;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Builder.BuildVehiclesTests
+{
+
+    [TestClass]
+    public class ShopTests
+    {
+        [TestMethod]
+        public void When_Scooter_IsBuild()
+        {
+            var shop = new Shop();
+            shop.Construct(new ScooterBuilder());
+        }
+
+        [TestMethod]
+        public void When_MotorCycle_IsBuild()
+        {
+            var shop = new Shop();
+            shop.Construct(new MotorCycleBuilder());
+        }
+
+        [TestMethod]
+        public void When_Car1_IsBuild()
+        {
+            var shop = new Shop();
+            shop.Construct(new CarBuilder());
+        }
+    }
+
+}
