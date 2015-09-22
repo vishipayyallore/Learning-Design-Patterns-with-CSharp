563795e7ac769f9ae889ade91eddac72b2c0205b
vishipayyallore <vishipayyallore@yahoo.com>
2015-09-22 21:50:37 +0530

    After V2' Consumer class is completed.

---
+++ DesignPatternsWithCSharp/GOFDesignPatterns/Builder.BuildVehiclesV2Consumer/BuildVehiclesV2.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Builder.BuildVehiclesV2.ConcreteBuilder;
+using Builder.BuildVehiclesV2.Director;
+
+namespace Builder.BuildVehiclesV2Consumer
+{
+    public class BuildVehiclesV2
+    {
+        static void Main()
+        {
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            try
+            {
+                var shop = new Shop();
+                shop
+                    .Construct(new ScooterBuilder())
+                    .Construct(new MotorCycleBuilder())
+                    .Construct(new CarBuilder());
+            }
+            catch (Exception error)
+            {
+                Console.WriteLine("\n\nError occured at BuildVehicles::Main(). Message: {0}", error.Message);
+            }
+            Console.WriteLine("\n\nPress any key ...");
+            Console.ReadKey();
+        }
+
+    }
+}
