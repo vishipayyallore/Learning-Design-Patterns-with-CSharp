e70045ac5b309d801c5d4de2e15a7365f42fff6f
vishipayyallore <vishipayyallore@yahoo.com>
2015-09-23 23:17:50 +0530

    AAA

---
+++ DesignPatternsWithCSharp/MongoDbDataStore/MongoDb.DataStore.Utilities/ConfigurationHelper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Configuration;
+
+namespace MongoDb.DataStore.Utilities
+{
+    public class ConfigurationHelper
+    {
+
+        #region Methdos.
+        public static string GetConfigurationValue(string configurationItemKey)
+        {
+            return ConfigurationManager.AppSettings[configurationItemKey] ?? string.Empty;
+        }
+        #endregion
+
+    }
+
+}
