4108f89c50f742c26f96b4c636366d52de3eacaf
vishipayyallore <vishipayyallore@yahoo.com>
2015-09-24 18:34:05 +0530

    After adding additional Projects

---
+++ DesignPatternsWithCSharp/MongoDb.BuildVehicles.DataStore/BuildVehiclesContext.cs
@@ -0,0 +1,20 @@
+﻿namespace MongoDb.BuildVehicles.DataStore
+{
+
+    public class BuildVehiclesContext
+    {
+
+        private static IMongoDatabase _mongoDatabase;
+
+        public BuildVehiclesContext()
+        {
+            var mongoDbConnectionString = ConfigurationHelper.GetConfigurationValue("MongoDbConnectionString");
+            var mongoDbDatabaseName = ConfigurationHelper.GetConfigurationValue("MongoDbDatabaseName");
+
+            var mongoClient = new MongoClient(mongoDbConnectionString);
+            _mongoDatabase = mongoClient.GetDatabase(mongoDbDatabaseName);
+        }
+
+    }
+
+}
---
+++ DesignPatternsWithCSharp/MongoDbDataStore/MongoDb.DataStore/MongoDataStore.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Driver;
+
+namespace MongoDb.DataStore
+{
+    public class MongoDataStore
+    {
+
+        private static readonly MongoClient _MongoClient;
+
+        private MongoDataStore()
+        {
+        }
+
+        static MongoDataStore()
+        {
+            var mongoDbConnectionString = ConfigurationHelper.GetConfigurationValue("MongoDbConnectionString");
+            _MongoClient = new MongoClient(mongoDbConnectionString);
+        }
+        //var mongoClient = new MongoClient(mongoDbConnectionString);
+    }
+
+}
