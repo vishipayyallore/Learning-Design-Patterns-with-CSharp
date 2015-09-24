f72881d5109d9e7e0dc1e57d3f200215b6fe5d7b
vishipayyallore <vishipayyallore@yahoo.com>
2015-09-24 21:16:54 +0530

    Tests for Data Store completed

--- DesignPatternsWithCSharp/MongoDbDataStore/MongoData.DataStore.Tests/MongoDataStoreTests.cs
+++ DesignPatternsWithCSharp/MongoDbDataStore/MongoData.DataStore.Tests/MongoDataStoreTests.cs
@@ -17,5 +17,16 @@ namespace MongoData.DataStore.Tests
             Assert.AreEqual("MongoDataStore", className);
         }
 
+        [TestMethod]
+        public void When_DataStoreClient_IsRetrieved()
+        {
+            var dataStoreInstance = MongoDataStore.DataStoreInstance;
+            var datastoreClient = dataStoreInstance.MongoDataStoreClient;
+            var serverName = datastoreClient.Settings.Server.ToString();
+            Console.WriteLine("Server Name: {0}", serverName);
+            Assert.AreEqual("localhost:27017", serverName);
+        }
+
     }
+
 }
