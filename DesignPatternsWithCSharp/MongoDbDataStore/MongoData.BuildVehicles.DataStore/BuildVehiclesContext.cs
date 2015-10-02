f847b9295629dbc356a2201be20baff7c36f0ddc
vishipayyallore <vishipayyallore@yahoo.com>
2015-10-02 20:57:35 +0530

    AAA

--- DesignPatternsWithCSharp/MongoDbDataStore/MongoData.BuildVehicles.DataStore/BuildVehiclesContext.cs
+++ DesignPatternsWithCSharp/MongoDbDataStore/MongoData.BuildVehicles.DataStore/BuildVehiclesContext.cs
@@ -7,16 +7,19 @@ namespace MongoData.BuildVehicles.DataStore
 
     public class BuildVehiclesContext
     {
-
+        #region Properties.
         private static IMongoDatabase _mongoDatabase;
         private static readonly MongoDataStore MongoDataStoreInstance = MongoDataStore.DataStoreInstance;
+        #endregion
 
         public BuildVehiclesContext()
         {
             _mongoDatabase = MongoDataStoreInstance.DatabaseInstance;
         }
 
+        #region Properties
         public IMongoCollection<Door> Doors => _mongoDatabase.GetCollection<Door>("doors");
+        #endregion
     }
 
 }
