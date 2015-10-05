acedcfb0e57958559cdf6e31887101a66e08621d
vishipayyallore <vishipayyallore@yahoo.com>
2015-10-05 10:42:41 +0530

    AAA

---
+++ DesignPatterns/MongoData.BuildVehicles.DataRepository/DoorRepository.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using MongoData.DesignPattern.DataModels;
+using MongoData.IDataRepositories;
+
+namespace MongoData.BuildVehicles.DataRepository
+{
+    public class DoorRepository : IRepository<Door>
+    {
+
+        public DoorRepository() {  }
+
+        #region Properties
+        public IEnumerable<Door> List { get; }
+        #endregion
+
+        #region Properties
+        public void Add(Door entity)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Delete(Door entity)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Update(Door entity)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Door FindById(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+        #endregion
+    }
+}
---
+++ DesignPatterns/MongoData.BuildVehicles.DataRepository/DoorRepositoty.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoData.DesignPattern.DataModels;
+using MongoData.IDataRepositories;
+
+namespace MongoData.BuildVehicles.DataRepository
+{
+    public class DoorRepositoty : IRepository<Door>
+    {
+
+        public DoorRepositoty()
+        {
+        }
+
+        public IEnumerable<Door> List { get; }
+
+        public void Add(Door entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Door entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Door entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Door FindById(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
