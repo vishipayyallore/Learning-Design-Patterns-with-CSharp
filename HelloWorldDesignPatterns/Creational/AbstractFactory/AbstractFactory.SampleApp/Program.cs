09a794dc95661dd3d96905a96f618ae8d649ea66
Viswanatha Swamy <vishipayyallore@yahoo.com>
2016-11-21 17:23:36 +0530

    Abstract Factory Demo

---
+++ HelloWorldDesignPatterns/Creational/AbstractFactory/AbstractFactory.SampleApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using static System.Console;
+
+namespace AbstractFactory.SampleApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            ForegroundColor = ConsoleColor.Cyan;
+
+            WriteLine("\n\nPress any key ....");
+            ReadKey();
+        }
+    }
+}
